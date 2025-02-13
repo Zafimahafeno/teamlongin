864faf4cf546b09f73fe0cf2744047015ee697d0
Lucaseliade <lucaseliade@gmail.com>
2025-02-13 17:56:50 +0100

    Lucas

--- backend/totalNonTraite.php
+++ backend/totalNonTraite.php
@@ -7,22 +7,25 @@ if ($conn->connect_error) {
     die("Échec de la connexion à la base de données: " . $conn->connect_error);
 }
 
-// Requête SQL pour compter les votants "favorable"
+// Requête SQL pour compter les votants "Non traité"
 $sql_nontraite = "
     SELECT COUNT(*) AS total_nontraite
     FROM votant
-    WHERE intentionVote = 'Non traité' OR intentionVote = ''
+    WHERE (intentionVote = 'Non traité' OR intentionVote = '') 
+    AND (fonction = 'PAT' OR (fonction = 'Enseignant' AND tel <> ''))
 ";
 
-// Exécution de la requête pour "favorable"
+// Exécution de la requête
 $result_nontraite = $conn->query($sql_nontraite);
 
-// Vérification et affichage du résultat pour "favorable"
-if ($result_nontraite->num_rows > 0) {
-    $row_nontraite = $result_nontraite->fetch_assoc();
-    $total_nontraite = $row_nontraite["total_nontraite"];
-    echo $total_nontraite;  // Afficher directement le nombre
+// Vérification si la requête a échoué
+if (!$result_nontraite) {
+    die("Erreur SQL: " . $conn->error);  // Affiche l'erreur SQL
 }
+
+// Récupération et affichage du résultat
+$row_nontraite = $result_nontraite->fetch_assoc();
+echo $row_nontraite["total_nontraite"];  
 
 // Fermeture de la connexion
 $conn->close();
