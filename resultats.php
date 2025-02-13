786396532eb707f5610d120d14f1945dec60c02a
Lucaseliade <lucaseliade@gmail.com>
2025-02-13 17:48:04 +0100

    Lucas

--- resultats.php
+++ resultats.php
@@ -44,6 +44,13 @@ include './includes/sidebar.php';
 .btn-primary {
     width: 100%;
 }
+
+@media screen and (max-width: 800px) {
+    .container-custom {
+        display: flex;
+        flex-direction: column-reverse;
+    }
+}
 </style>
 
 <div class="content-wrapper">
@@ -56,7 +63,7 @@ include './includes/sidebar.php';
         </ol>
     </section>
 
-    <div class="container my-5">
+    <div class="container my-5 montable">
         <h2 class="text-center text-primary mb-4"></h2>
         <div class="container-custom">
             <!-- Tableau des résultats -->
@@ -73,55 +80,55 @@ include './includes/sidebar.php';
                     </thead>
                     <tbody>
                         <?php
-$conn = new mysqli("mysql-mahafeno.alwaysdata.net", "mahafeno", "antso0201", "mahafeno_longin");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+                            $conn = new mysqli("mysql-mahafeno.alwaysdata.net", "mahafeno", "antso0201", "mahafeno_longin");
+                            if ($conn->connect_error) {
+                                die("Connection failed: " . $conn->connect_error);
+                            }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $id_candidat = $_POST['id_candidat'];
-    $fonction_votant = $_POST['fonction_votant'];
-    $nombreVote = (int)$_POST['nombreVote'];
+                            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                                $id_candidat = $_POST['id_candidat'];
+                                $fonction_votant = $_POST['fonction_votant'];
+                                $nombreVote = (int)$_POST['nombreVote'];
 
-    // Vérification et mise à jour des votes dans la base de données
-    $queryCheck = "SELECT * FROM voix WHERE id_candidat = ? AND fonction_votant = ?";
-    $stmt = $conn->prepare($queryCheck);
-    $stmt->bind_param("is", $id_candidat, $fonction_votant);
-    $stmt->execute();
-    $resultCheck = $stmt->get_result();
+                                // Vérification et mise à jour des votes dans la base de données
+                                $queryCheck = "SELECT * FROM voix WHERE id_candidat = ? AND fonction_votant = ?";
+                                $stmt = $conn->prepare($queryCheck);
+                                $stmt->bind_param("is", $id_candidat, $fonction_votant);
+                                $stmt->execute();
+                                $resultCheck = $stmt->get_result();
 
-    if ($resultCheck->num_rows > 0) {
-        // Mise à jour des votes existants
-        $queryUpdate = "UPDATE voix SET nombreVote = nombreVote + ? WHERE id_candidat = ? AND fonction_votant = ?";
-        $stmt = $conn->prepare($queryUpdate);
-        $stmt->bind_param("iis", $nombreVote, $id_candidat, $fonction_votant);
-        $stmt->execute();
-    } else {
-        // Insertion des nouveaux votes
-        $queryInsert = "INSERT INTO voix (id_candidat, fonction_votant, nombreVote) VALUES (?, ?, ?)";
-        $stmt = $conn->prepare($queryInsert);
-        $stmt->bind_param("isi", $id_candidat, $fonction_votant, $nombreVote);
-        $stmt->execute();
-    }
-}
+                                if ($resultCheck->num_rows > 0) {
+                                    // Mise à jour des votes existants
+                                    $queryUpdate = "UPDATE voix SET nombreVote = nombreVote + ? WHERE id_candidat = ? AND fonction_votant = ?";
+                                    $stmt = $conn->prepare($queryUpdate);
+                                    $stmt->bind_param("iis", $nombreVote, $id_candidat, $fonction_votant);
+                                    $stmt->execute();
+                                } else {
+                                    // Insertion des nouveaux votes
+                                    $queryInsert = "INSERT INTO voix (id_candidat, fonction_votant, nombreVote) VALUES (?, ?, ?)";
+                                    $stmt = $conn->prepare($queryInsert);
+                                    $stmt->bind_param("isi", $id_candidat, $fonction_votant, $nombreVote);
+                                    $stmt->execute();
+                                }
+                            }
 
-$queryTotalVotants = "SELECT (SELECT COUNT(*) FROM votant WHERE fonction = 'PAT') AS totalVotantsPAT,(SELECT COUNT(*) FROM votant WHERE fonction = 'Enseignant' AND tel <>'') AS totalVotantsEnseignant";
-$resultTotalVotants = $conn->query($queryTotalVotants);
-$rowTotalVotants = $resultTotalVotants->fetch_assoc();
+                            $queryTotalVotants = "SELECT (SELECT COUNT(*) FROM votant WHERE fonction = 'PAT') AS totalVotantsPAT,(SELECT COUNT(*) FROM votant WHERE fonction = 'Enseignant' AND tel <>'') AS totalVotantsEnseignant";
+                            $resultTotalVotants = $conn->query($queryTotalVotants);
+                            $rowTotalVotants = $resultTotalVotants->fetch_assoc();
 
-$totalVotantsPAT = $rowTotalVotants['totalVotantsPAT'];
-$totalVotantsEnseignant = $rowTotalVotants['totalVotantsEnseignant'];
+                            $totalVotantsPAT = $rowTotalVotants['totalVotantsPAT'];
+                            $totalVotantsEnseignant = $rowTotalVotants['totalVotantsEnseignant'];
 
-$queryCandidats = "
-    SELECT 
-        candidat.id, 
-        candidat.nom,
-        COALESCE(SUM(CASE WHEN fonction_votant = 'PAT' THEN nombreVote ELSE 0 END), 0) AS voixPAT,
-        COALESCE(SUM(CASE WHEN fonction_votant = 'Enseignant' THEN nombreVote ELSE 0 END), 0) AS voixEnseignant
-    FROM candidat
-    LEFT JOIN voix ON candidat.id = voix.id_candidat
-    GROUP BY candidat.id
-";
+                            $queryCandidats = "
+                                SELECT 
+                                    candidat.id, 
+                                    candidat.nom,
+                                    COALESCE(SUM(CASE WHEN fonction_votant = 'PAT' THEN nombreVote ELSE 0 END), 0) AS voixPAT,
+                                    COALESCE(SUM(CASE WHEN fonction_votant = 'Enseignant' THEN nombreVote ELSE 0 END), 0) AS voixEnseignant
+                                FROM candidat
+                                LEFT JOIN voix ON candidat.id = voix.id_candidat
+                                GROUP BY candidat.id
+                            ";
                         $resultCandidats = $conn->query($queryCandidats);
                         
                         if ($resultCandidats->num_rows > 0) {
